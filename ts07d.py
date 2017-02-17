218ed257b3a4c364c6c3960a264f5251ef622d89
vmerkin <slava.merkin@jhuapl.edu>
2017-02-17 12:01:24 -0500

    Forgot to rename rmin to rcutoff everywhere.

--- ts07d.py
+++ ts07d.py
@@ -1,7 +1,7 @@
 from numpy import loadtxt,meshgrid,sqrt,savetxt,vstack
 import numpy.ma as ma
 
-def get_data(xFileName,yFileName,jxbxFileName,jxbyFileName,baddata=-1.e4,rcutoff=1.5,xflip=xFlip):
+def get_data(xFileName,yFileName,jxbxFileName,jxbyFileName,baddata=-1.e4,rcutoff=1.5,xflip=False):
     y=loadtxt(yFileName); Ny = y.size
     if xflip:
         x=loadtxt(xFileName)[::-1]; Nx = x.size   # note the axis flip to get ascending order
@@ -18,8 +18,8 @@ def get_data(xFileName,yFileName,jxbxFileName,jxbyFileName,baddata=-1.e4,rcutoff
     jxbx = ma.masked_equal(jxbx,baddata)
     jxby = ma.masked_equal(jxby,baddata)
 
-    jxbx = ma.masked_where(r<=rmin,jxbx)
-    jxby = ma.masked_where(r<=rmin,jxby)
+    jxbx = ma.masked_where(r<=rcutoff,jxbx)
+    jxby = ma.masked_where(r<=rcutoff,jxby)
 
     return(X,Y,jxbx,jxby)
 
